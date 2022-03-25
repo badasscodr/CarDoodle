9eea9a1aab52772da33ccd0391d3eac5794c75f9
badasscoder <noreply@badasscoder.mail>
2022-03-25 20:08:26 +0500

    uploading years old project to git

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("nav-wrapper").style.transform = "translate(0, 180px)";
+}
+function closeNav() {
+    document.getElementById("nav-wrapper").style.transform = "translate(296px, 180px)";
+}
